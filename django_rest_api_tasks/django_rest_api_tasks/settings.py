d2d287e88ee7b516c3ed9ec3a113b6076b72ee41
justin <justinwmckelvey@gmail.com>
2022-09-21 18:26:52 -0500

    remove keys

--- django_rest_api_tasks/django_rest_api_tasks/settings.py
+++ django_rest_api_tasks/django_rest_api_tasks/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-k=dk*gqz*c_5dfq2#5&8$cx)x2b_vsp(4(v=0z)cphdg0iics4"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
